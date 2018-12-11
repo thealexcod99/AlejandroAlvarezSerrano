705cb7215356fef583df9c89c4a5dac7dbe7f04e
Alejandro A. <alex@gmail.com>
2018-12-11 14:34:18 +0100

    Commit con documentación y fase final

--- src/CalcularAreas/CalcularAreas.java
+++ src/CalcularAreas/CalcularAreas.java
@@ -1,7 +1,8 @@
 package CalcularAreas;
 
 public class CalcularAreas {
-
+	
+	//Método void que genera el menú.
 	public static void menu() {
 		
 		System.out.println("¿De qué figura deseas calcular el área?");
@@ -13,17 +14,18 @@ public class CalcularAreas {
 		
 	}
 	
+	//Método que calcula el área del triángulo y la retorna.
 	public static double areaTriangulo( int base, int altura ) {
 		return base * altura / 2.0;
 	}
 	
+	//Método que calcula el área del rectángulo y la retorna.
 	public static int areaRectangulo( int base, int altura ) {
 		return base * altura;
 	}
 	
+	//Método que calcula el área del trapecio y la retorna.
 	public static double areaTrapecio( int base1, int base2, int altura ) {
 		return altura * ((base1 + base2) / 2.0);
 	}
-	
-	
 }
--- src/CalcularAreas/TestCalcularAreas.java
+++ src/CalcularAreas/TestCalcularAreas.java
@@ -78,6 +78,6 @@ public class TestCalcularAreas {
 			}
 		}
 		sc.close();
+		System.exit(3);
 	}
-
 }
