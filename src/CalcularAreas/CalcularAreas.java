687987ad4a0c1373ec395800c650de91127550bd
Alejandro A. <alex@gmail.com>
2018-12-11 14:22:51 +0100

    Commit final

--- src/CalcularAreas/CalcularAreas.java
+++ src/CalcularAreas/CalcularAreas.java
@@ -5,9 +5,9 @@ public class CalcularAreas {
 	public static void menu() {
 		
 		System.out.println("¿De qué figura deseas calcular el área?");
-		System.out.println("1. Triángulo.");
-		System.out.println("2. Rectángulo.");
-		System.out.println("3. Trapecio.");
+		System.out.println("-Triángulo (Pulsa 1).");
+		System.out.println("-Rectángulo (Pulsa 2).");
+		System.out.println("-Trapecio (Pulsa 3).");
 		System.out.println("");
 		System.out.println("Para salir del programa pulsa 4.");
 		
--- src/CalcularAreas/TestCalcularAreas.java
+++ src/CalcularAreas/TestCalcularAreas.java
@@ -6,23 +6,78 @@ public class TestCalcularAreas {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		CalcularAreas.menu();
+		boolean salir = true;
 		
-		int opcion = sc.nextInt();
+		while (salir) {
 		
-		if (opcion == 1) {
-			System.out.println(CalcularAreas.areaTriangulo(sc.nextInt(), sc.nextInt()));
+			CalcularAreas.menu();
+			System.out.print("Opción: ");	
+			int opcion = sc.nextInt();
+		
+			switch (opcion) {
+					
+				case 1:
+					System.out.print("Introduce la base del triángulo: ");
+					int baset = sc.nextInt();
+					
+					if (baset <= 0) {
+						System.out.println("El valor introducido no es válido");
+						System.exit(1);
+					}
+					
+					System.out.print("Introduce la altura del triángulo: ");
+					int alturat = sc.nextInt();
+					if (alturat <= 0) {
+						System.out.println("El valor introducido no es válido");
+						System.exit(1);
+					}
+					
+					System.out.println("El área del triángulo es " + CalcularAreas.areaTriangulo(baset, alturat));
+					System.out.println("");
+					break;
+				
+				case 2:
+					System.out.print("Introduce la base del rectángulo: ");
+					int baser = sc.nextInt();
+					if (baser < 2 || baser > 10) {
+						System.out.println("El valor introducido no es válido");
+						System.exit(1);
+					}
+					
+					System.out.print("Introduce la altura del rectángulo: ");
+					int alturar = sc.nextInt();
+					if (alturar < 2 || alturar > 10) {
+						System.out.println("El valor introducido no es válido");
+						System.exit(1);
+					}
+					
+					System.out.println("El área del rectángulo es " + CalcularAreas.areaRectangulo(baser, alturar));
+					System.out.println("");
+					break;
+				
+				case 3:
+					System.out.print("Introduce la base inferior del trapecio: ");
+					int basetr1 = sc.nextInt();					
+					System.out.print("Introduce la base superior del trapecio: ");
+					int basetr2 = sc.nextInt();
+					if (basetr1 == basetr2) {
+						System.out.println("Las bases no pueden ser iguales");
+						System.exit(2);
+					}
+					
+					System.out.print("Introduce la altura del trapecio: ");
+					int alturatr = sc.nextInt();
+					
+					System.out.println("El área del trapecio es " + CalcularAreas.areaTrapecio(basetr1, basetr2, alturatr));
+					System.out.println("");
+					break;
+				
+				case 4:
+					salir = false;
+					break;
+			}
 		}
-		else if (opcion == 2) {
-			System.out.println(CalcularAreas.areaRectangulo(sc.nextInt(), sc.nextInt()));
-		}
-		else if (opcion == 3) {
-			System.out.println(CalcularAreas.areaTrapecio(sc.nextInt(), sc.nextInt(), sc.nextInt()));
-		}
-		else if (opcion == 4) {
-			System.exit(1);
-		}
-
+		sc.close();
 	}
 
 }
